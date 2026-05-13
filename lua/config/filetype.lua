1f134fe5d8d910ce1f73195d4f404b73142a797d
noritugi <noritsugi@vuwuv.com>
2026-05-14 05:02:48 +0900

    Powershellスクリプト(.ps1)を開いた時にBOMを付けるようにした

--- lua/config/filetype.lua
+++ lua/config/filetype.lua
@@ -20,3 +20,28 @@ vim.api.nvim_create_autocmd("FileType", {
     end
   end
 })
+
+-- PowerShell(.ps1) を開いた時、
+-- BOM が無ければ UTF-8 with BOM に設定する
+-- 保存はしない
+local group = vim.api.nvim_create_augroup("PowerShellUtf8Bom", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  group = group,
+  pattern = "*.ps1",
+  callback = function(args)
+    local buf = args.buf
+
+    -- 既に BOM 付きなら何もしない
+    if vim.bo[buf].bomb then
+      return
+    end
+
+    -- UTF-8 with BOM に設定
+    vim.bo[buf].fileencoding = "utf-8"
+    vim.bo[buf].bomb = true
+
+    -- buffer を変更済みにしない
+    vim.bo[buf].modified = false
+  end,
+})
