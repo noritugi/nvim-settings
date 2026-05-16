4cb02b4f6198a2884545876a20b82229fb32e3b4
乗継 <noritsugi@vuwuv.com>
2026-05-16 17:32:04 +0900

    lazydev.nvimをインストールした NeovideのIMEでの入力をオフにした

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -10,3 +10,13 @@ vim.keymap.set("v", "<C-c>", copy, { silent = true, desc = "Copy" })
 vim.keymap.set({ "n", "i", "v", "c", "t" }, "<C-v>", paste, { silent = true, desc = "Paste" })
 -- フォント
 vim.o.guifont = "HackGen Console NF:h12"
+-- タイトルバー
+vim.g.neovide_title_background_color = string.format(
+    "%x",
+    vim.api.nvim_get_hl(0, {id=vim.api.nvim_get_hl_id_by_name("Normal")}).bg
+)
+vim.g.neovide_title_text_color = "pink"
+-- ウィンドウコーナー処理
+vim.g.neovide_corner_preference = "round"
+-- skkeletonを使うためIMEはオフ
+vim.g.neovide_input_ime = false
