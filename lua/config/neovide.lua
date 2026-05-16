2e09462d5f42fdbb0572ef3340eb321522efbc63
乗継 <noritsugi@vuwuv.com>
2026-05-16 12:12:01 +0900

    クリップボードと保存のキーバインドを変更した

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,8 +1,12 @@
 --
 -- Neovide用の設定ファイル
 --
--- クリップボード
-vim.keymap.set({ "n", "x" }, "<C-S-C>", '"+y', { desc = "システムのクリップボードをコピー" })
-vim.keymap.set({ "n", "x" }, "<C-S-V>", '"+p', { desc = "システムのクリップボードをペースト" })
+-- クリップボードとCtrl-Sで保存
+local function save() vim.cmd.write() end
+local function copy() vim.cmd([[normal! "+y]]) end
+local function paste() vim.api.nvim_paste(vim.fn.getreg("+"), true, -1) end
+vim.keymap.set({ "n", "i", "v" }, "<C-s>", save, { desc = "Save" })
+vim.keymap.set("v", "<C-c>", copy, { silent = true, desc = "Copy" })
+vim.keymap.set({ "n", "i", "v", "c", "t" }, "<C-v>", paste, { silent = true, desc = "Paste" })
 -- フォント
 vim.o.guifont = "HackGen Console NF:h12"
